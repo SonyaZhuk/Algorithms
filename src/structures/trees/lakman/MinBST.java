f46ce441807152c34f11efa6d1cd992377b67350
Sofya Zhuk <sonya.zhook@mail.ru>
2023-12-05 16:54:34 +0300

    was added linkedlist for nodes

--- src/structures/trees/lakman/MinBST.java
+++ src/structures/trees/lakman/MinBST.java
@@ -1,5 +1,8 @@
 package structures.trees.lakman;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
  * Trees and Graphs.
  * <p>
@@ -7,9 +10,15 @@ package structures.trees.lakman;
  */
 public class MinBST {
 
+    /**
+     * Create min bst
+     * <p>
+     * See Lakman p. 250
+     */
     public TreeNode createMinimalBST(int array[]) {
         return createMinimalBST(array, 0, array.length - 1);
     }
+
     private TreeNode createMinimalBST(int arr[], int start, int end) {
         if (end < start) return null;
 
@@ -19,5 +28,57 @@ public class MinBST {
         node.left = createMinimalBST(arr, start, mid - 1);
         node.right = createMinimalBST(arr, mid + 1, end);
         return node;
+    }
+
+    /**
+     * Create linkedlist of nodes for each level, recursion ver, O(N) time, O(N) memory
+     * <p>
+     * See Lakman p. 251
+     */
+    public ArrayList<LinkedList<TreeNode>> createLevelLinkedListRec(TreeNode root) {
+        ArrayList<LinkedList<TreeNode>> lists = new ArrayList<>();
+        createLevelLinkedList(root, lists, 0);
+        return lists;
+    }
+    private void createLevelLinkedList(TreeNode root, ArrayList<LinkedList<TreeNode>> lists, int level) {
+        if (root == null) return;
+
+        LinkedList<TreeNode> list;
+        if (lists.size() == level) { // level is not in the lists
+            list = new LinkedList<>();
+            /* Уровни всегда обходятся по порядку.
+            Следовательно, при первом посещении уровня i уровни с 0 по i-1 уже должны быть просмотрены,
+            поэтому новый уровень можно безопасно добавить в конец. */
+            lists.add(list);
+        } else {
+            list = lists.get(level);
+        }
+        list.add(root);
+        createLevelLinkedList(root.left, lists, level + 1);
+        createLevelLinkedList(root.right, lists, level + 1);
+    }
+    public ArrayList<LinkedList<TreeNode>> createLevelLinkedList(TreeNode root) {
+        ArrayList<LinkedList<TreeNode>> result = new ArrayList<>();
+
+        LinkedList<TreeNode> current = new LinkedList<>();
+        if (root != null) {
+            current.add(root);
+        }
+
+        while (current.size() > 0) {
+            result.add(current); //add previous level
+            LinkedList<TreeNode> parents = current; // move to the next level
+            current = new LinkedList<>();
+            for (TreeNode parent : parents) {
+                /* visiting child nodes */
+                if (parent.left != null) {
+                    current.add(parent.left);
+                }
+                if (parent.right != null) {
+                    current.add(parent.right);
+                }
+            }
+        }
+        return result;
     }
 }
