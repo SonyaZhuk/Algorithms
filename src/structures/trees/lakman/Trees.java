02163d325c969c93173c2eac27db3f6d74007032
Sofya Zhuk <sonya.zhook@mail.ru>
2023-12-06 13:51:31 +0300

    was added method that find the next node for the current

--- src/structures/trees/lakman/Trees.java
+++ src/structures/trees/lakman/Trees.java
@@ -177,4 +177,37 @@ public class Trees {
         }
         return true;
     }
+
+    /**
+     * Finds the next node for current node.
+     * <p>
+     * See Lakman p. 257
+     */
+    public TreeNode nextNode(TreeNode n) {
+        if (n == null) return null;
+
+        /* Found the right child node -> return the far left node of right subtree */
+        if (n.right != null) {
+            return leftMostChild(n.right);
+        } else {
+            TreeNode q = n;
+            TreeNode x = q.parent;
+            // Move above while we will stay to the left
+            while (x != null && x.left != q) {
+                q = x;
+                x = x.parent;
+
+            }
+            return x;
+        }
+    }
+    private TreeNode leftMostChild(TreeNode n) {
+        if (n == null) {
+            return null;
+        }
+        while (n.left != null) {
+            n = n.left;
+        }
+        return n;
+    }
 }
