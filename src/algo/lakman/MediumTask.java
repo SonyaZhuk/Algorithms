b1c04bfec28493a0e133bce1c8a5d03e7190337c
Sofya Zhuk <sonya.zhook@mail.ru>
2023-12-08 11:06:47 +0300

    was added medium task 2

--- src/algo/lakman/MediumTask.java
+++ src/algo/lakman/MediumTask.java
@@ -1,5 +1,8 @@
 package algo.lakman;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Medium level tasks.
  * <p>
@@ -29,6 +32,27 @@ public class MediumTask {
         y = x ^ y;
         x = x ^ y;
         System.out.println(x + " " + y);
+    }
+
+    /**
+     * Count a word frequency.
+     * <p>
+     * See Lakman p. 490
+     */
+    public int getFrequency(String[] words, String word) {
+        if (words == null || word == null) return -1;
+        final Map<String, Integer> dict = fillDict(words);
+        final String s = word.trim().toLowerCase();
+        return dict.getOrDefault(s, 0);
+    }
+
+    private Map<String, Integer> fillDict(String[] words) {
+        final Map<String, Integer> dict = new HashMap<>();
+        for (String word: words) {
+            final String s = word.trim().toLowerCase();
+            dict.put(s, dict.getOrDefault(s, 1) + 1);
+        }
+        return dict;
     }
 
     public static void main(String[] args) {
