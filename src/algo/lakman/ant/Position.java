ea2b1f2f13a19e8cb9ac5ddfea6f80c517f01ed7
Sofya Zhuk <sonya.zhook@mail.ru>
2023-12-14 14:26:33 +0300

    was added medium task 22

---
+++ src/algo/lakman/ant/Position.java
@@ -0,0 +1,48 @@
+package algo.lakman.ant;
+
+import java.util.Objects;
+
+public class Position {
+    private int row;
+    private int column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+
+
+   public Position clone() {
+       return new Position(row, column);
+   }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return row == position.row && column == position.column;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column);
+    }
+}
---
+++ src/algo/lakman/ant/board/Board.java
@@ -0,0 +1,84 @@
+package algo.lakman.ant.board;
+
+import algo.lakman.ant.Ant;
+import algo.lakman.ant.Position;
+
+import java.util.HashSet;
+
+/**
+ * Ant moving.
+ * <p>
+ * See Lakman p. 546
+ */
+public class Board {
+    private HashSet<Position> whites;
+    private Ant ant;
+    private Position topLeftCorner;
+    private Position bottomRightCorner;
+
+    public Board() {
+        this.whites = new HashSet<>();
+        this.ant = new Ant();
+        this.topLeftCorner = new Position(0, 0);
+        this.bottomRightCorner = new Position(0, 0);
+    }
+
+    public void move() {
+        ant.turn(isWhite(ant.getPosition())); // Поворот
+        flip(ant.getPosition()); // Изменение цвета
+        ant.move(); // move
+        ensureFit(ant.getPosition());
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        int rowMin = topLeftCorner.getRow();
+        int rowMax = bottomRightCorner.getRow();
+        int colMin = topLeftCorner.getColumn();
+        int colMax = bottomRightCorner.getColumn();
+        for (int r = rowMin; r <= rowMax; r++) {
+            for (int c = colMin; c <= colMax; c++) {
+                if (r == ant.getPosition().getRow() && c == ant.getPosition().getColumn()) {
+                    sb.append(ant.getOrientation());
+                } else if (isWhite(r, c)) {
+                    sb.append("X");
+                } else {
+                    sb.append("_");
+                }
+            }
+            sb.append("\n");
+        }
+        sb.append("Ant: " + ant.getOrientation() + "\n");
+        return sb.toString();
+    }
+
+    /* Изменение цвета ячеек. */
+    private void flip(Position position) {
+        if (whites.contains(position)) {
+            whites.remove(position);
+        } else {
+            whites.add(position.clone());
+        }
+    }
+
+    /*Проверка границ (левый верхний и правый нижний угол).*/
+    private void ensureFit(Position position) {
+        int row = position.getRow();
+        int column = position.getColumn();
+
+        topLeftCorner.setRow(Math.min(topLeftCorner.getRow(), row));
+        topLeftCorner.setColumn(Math.min(topLeftCorner.getColumn(), column));
+
+        bottomRightCorner.setRow(Math.max(bottomRightCorner.getRow(), row));
+        bottomRightCorner.setColumn(Math.max(bottomRightCorner.getColumn(), column));
+    }
+
+    /* Проверяет, является ли ячейка белой. */
+    private boolean isWhite(Position pos) {
+        return whites.contains(pos);
+    }
+
+    private boolean isWhite(int row, int column) {
+        return whites.contains(new Position(row, column));
+    }
+}
