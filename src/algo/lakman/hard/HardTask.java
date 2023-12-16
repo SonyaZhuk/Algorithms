2db115351ec2b4410302576ac03795e938cc8fb6
Sofya Zhuk <sonya.zhook@mail.ru>
2023-12-16 11:25:02 +0300

    added hard task 2

--- src/algo/lakman/hard/HardTask.java
+++ src/algo/lakman/hard/HardTask.java
@@ -29,4 +29,34 @@ public class HardTask {
         }
         return a;
     }
+
+    /**
+     * Shuffle 52 cards.
+     * <p>
+     * See Lakman p. 562
+     */
+    public int[] shuffleArrayRecursively(int[] cards, int i) {
+        if (i == 0) return cards;
+        shuffleArrayRecursively(cards, i - 1); // Перетасовать предыдущую часть
+        int k = rand(0, i); // Выбрать случайный индекс
+        /* Переставить местами элементы k и i */
+        int temp = cards[k];
+        cards[k] = cards[i];
+        cards[i] = temp;
+        return cards;
+    }
+
+    /* Случайное число в диапазоне от lower до higher включительно */
+    private int rand(int lower, int higher) {
+        return lower + (int) (Math.random() * (higher - lower + 1));
+    }
+
+    public void shuffleIteratively(int[] cards) {
+        for (int i = 0; i < cards.length; i++) {
+            int k = rand(0, i);
+            int temp = cards[k];
+            cards[k] = cards[i];
+            cards[i] = temp;
+        }
+    }
 }
