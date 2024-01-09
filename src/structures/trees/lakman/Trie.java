61595dfaa05c91d1e68e007d28ee983c11e630ad
Sofya Zhuk <sonya.zhook@mail.ru>
2024-01-09 18:08:17 +0300

    was added trie tree Lakman

--- src/structures/trees/lakman/Trie.java
+++ src/structures/trees/lakman/Trie.java
@@ -1,13 +1,12 @@
-package structures.trees;
+package structures.trees.lakman;
 
 public class Trie {
-
-    static class TrieNode {
+    public static class TrieNode {
         TrieNode[] children = new TrieNode[128];
         boolean leaf;
     }
 
-    public static void insertString(TrieNode root, String s) {
+    public void insertString(TrieNode root, String s) {
         TrieNode v = root;
         for (char ch : s.toCharArray()) {
             TrieNode next = v.children[ch];
@@ -18,7 +17,7 @@ public class Trie {
         v.leaf = true;
     }
 
-    public static void printSorted(TrieNode node, String s) {
+    public void printSorted(TrieNode node, String s) {
         for (char ch = 0; ch < node.children.length; ch++) {
             TrieNode child = node.children[ch];
             if (child != null)
@@ -29,12 +28,15 @@ public class Trie {
         }
     }
 
-    // Usage example
+    /**
+     * Unit tests the <tt>Trie tree</tt> data type.
+     */
     public static void main(String[] args) {
-        TrieNode root = new TrieNode();
-        insertString(root, "hello");
-        insertString(root, "world");
-        insertString(root, "hi");
-        printSorted(root, "");
+        Trie trie = new Trie();
+        TrieNode node = new TrieNode();
+        trie.insertString(node, "hello");
+        trie.insertString(node, "world");
+        trie.insertString(node, "hi");
+        trie.printSorted(node, "");
     }
 }
